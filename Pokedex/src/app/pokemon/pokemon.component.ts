e14c25270592165b06bfc5b0db31bea38bd1c8b9
aurelie661 <aurelbout59@gmail.com>
2023-03-07 16:52:58 +0100

    recuperation donnée de l'input=affichage des données de l'objet

--- Pokedex/src/app/pokemon/pokemon.component.ts
+++ Pokedex/src/app/pokemon/pokemon.component.ts
@@ -8,13 +8,15 @@ import { PokemonServiceService } from '../pokemon-service.service';
 })
 export class PokemonComponent implements OnInit{
   pokemonId:number= 0;
+  data:string="";
 
   constructor(private pokemonService : PokemonServiceService){};
   
   getValue(){
     this.pokemonService.getPokemon(this.pokemonId).subscribe({
-      next: (response) => {
-        console.log(response);     
+      next: (response) => {    
+        let data = response;
+        console.log(data); 
       },
       error:(error) =>{
         console.log("Une erreur est survenu!");       
