702f722cb3b835e184276eb1af8b977994e09306
aurelie661 <boutaurel59@gmail.com>
2023-06-02 12:57:43 +0200

    remove detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
-    PokemonComponent
+    PokemonComponent,
   ],
   imports: [
     HttpClientModule,
